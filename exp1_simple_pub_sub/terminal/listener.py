ae20e27a24fb115cb944b2d7d4fdad19171f6454
shittu0507 <shittumanishit@gmail.com>
2025-10-29 20:36:12 +0530

    Added exp1: Simple Publisher and Subscriber with terminal steps

---
+++ exp1_simple_pub_sub/terminal/listener.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(f"I heard: {data.data}")
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('chatter', String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
